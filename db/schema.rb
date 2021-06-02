15aeb7cc2ec9abdafd2d6dfade83de9fa3e502d0
Vincenzo <vinzgameslol@gmail.com>
2021-06-02 10:51:09 +0200

    Correctly created events reference to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_175117) do
+ActiveRecord::Schema.define(version: 2021_06_02_082344) do
 
   create_table "events", force: :cascade do |t|
     t.date "event_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_06_01_175117) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
