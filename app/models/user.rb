6eaf8c23e40bb9201f626b7cd73031a968580db0
Vincenzo <vinzgameslol@gmail.com>
2021-06-09 11:49:07 +0200

    Added separation for past and future events in user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :events
   has_many :invitations
-  has_many :attended_event, through: :invitations, class_name: 'Event', foreign_key: 'event_id'
+  has_many :attended_event, through: :invitations, class_name: 'Event', foreign_key: 'event_id', source: :event
 end
